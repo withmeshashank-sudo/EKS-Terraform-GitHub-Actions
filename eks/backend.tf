2e00dfd242e23bca1bf8a307f5a2aa199ad331a4
withmeshashank-sudo <withmeshashank@gmail.com>
2026-09-04 16:13:38 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket294"
+    bucket         = "dev-aman-tf-bucket-294"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
